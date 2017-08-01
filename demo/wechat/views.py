7d02d62cbb136154e9d5abe14e60f4bbda1507b5
Nobody <nobody@bx220.onlinemgr>
2017-08-01 19:06:31 +0800

    git create init version 1

---
+++ demo/wechat/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse 
+from django.template import RequestContext
+
+from django.views.decorators.csrf import csrf_exempt
+
+import time
+
+import hashlib
+
+TOKEN = "mysaesite" #必须与微信Token一致
+
+
+@csrf_exempt
+defwechat(request):
+    if request.method == 'GET':
+        response = HttpResponse(checkSignature(request),content_type="text/plain")
+        return response
+
+    else:
+        return None
+
+def checkSignature(request):
+    global TOKEN
+    signature = request.GET.get("signature", None)
+    timestamp = request.GET.get("timestamp", None)
+    nonce = request.GET.get("nonce", None)
+    echoStr = request.GET.get("echostr",None)
+    token = TOKEN
+    tmpList = [token,timestamp,nonce]
+    tmpList.sort()
+    tmpstr = "%s%s%s" % tuple(tmpList)
+    tmpstr = hashlib.sha1(tmpstr).hexdigest()
+    if tmpstr == signature:
+        return echoStr
+    else:
+        return None
