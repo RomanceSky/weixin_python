5ecd811d19c3cf439f25329af1b66180efe7dad2
Nobody <nobody@yq150.onlinetools>
2017-08-01 19:41:50 +0800

    git commit by online edit file 1/demo/wechat/views.py

--- demo/wechat/views.py
+++ demo/wechat/views.py
@@ -1,42 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse 
-from django.template import RequestContext
-
-from django.views.decorators.csrf import csrf_exempt
-
-import time
-
-import hashlib
-
-TOKEN = "mysaesite" #必须与微信Token一致
-
-
-@csrf_exempt
-defwechat(request):
-    if request.method == 'GET':
-        response = HttpResponse(checkSignature(request),content_type="text/plain")
-        return response
-
-    else:
-        return None
-
-def checkSignature(request):
-    global TOKEN
-    signature = request.GET.get("signature", None)
-    timestamp = request.GET.get("timestamp", None)
-    nonce = request.GET.get("nonce", None)
-    echoStr = request.GET.get("echostr",None)
-    token = TOKEN
-    tmpList = [token,timestamp,nonce]
-    tmpList.sort()
-    tmpstr = "%s%s%s" % tuple(tmpList)
-    tmpstr = hashlib.sha1(tmpstr).hexdigest()
-    if tmpstr == signature:
-        return echoStr
-    else:
-        return None
+def wexin(request):
+    """
+    所有的消息都会先进入这个函数进行处理，函数包含两个功能，
+    微信接入验证是GET方法，
+    微信正常的收发消息是用POST方法。
+    """
+    # 这个WEIXIN_TOKEN是在测试号的配置页面中配置的，等会会讲到
+    WEIXIN_TOKEN = 'tangzongyu'
+    if request.method == "GET":
+        signature = request.GET.get("signature", None)
+        timestamp = request.GET.get("timestamp", None)
+        nonce = request.GET.get("nonce", None)
+        echostr = request.GET.get("echostr", None)
+        token = WEIXIN_TOKEN
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("error")
