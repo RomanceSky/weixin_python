150e8abcff329961f0fdfc1047a47d1960579bf2
Nobody <nobody@yq150.onlinetools>
2017-08-05 21:00:14 +0800

    git commit by online edit file 1/receive.py

--- receive.py
+++ receive.py
@@ -5,11 +5,13 @@ import xml.etree.ElementTree as ET
 def parse_xml(web_data):
     if len(web_data) == 0:
         return None
-    xml = etree.fromstring(str_xml)#进行XML解析
+    #xml = etree.fromstring(str_xml)#进行XML解析
+    '''
     content=xml.find("Content").text#获得用户所输入的内
     msgType=xml.find("MsgType").text
     fromUser=xml.find("FromUserName").text
     toUser=xml.find("ToUserName").text
+    '''
     xmlData = ET.fromstring(web_data)
     msg_type = xmlData.find('MsgType').text
     if msg_type == 'text':
@@ -23,15 +25,16 @@ class Msg(object):
         self.FromUserName = xmlData.find('FromUserName').text
         self.CreateTime = xmlData.find('CreateTime').text
         self.MsgType = xmlData.find('MsgType').text
-        self.MsgId = xmlData.find('MsgId').text
+        #self.MsgId = xmlData.find('MsgId').text
 
 class TextMsg(Msg):
     def __init__(self, xmlData):
         Msg.__init__(self, xmlData)
         self.Content = xmlData.find('Content').text.encode("utf-8")
-
+'''
 class ImageMsg(Msg):
     def __init__(self, xmlData):
         Msg.__init__(self, xmlData)
         self.PicUrl = xmlData.find('PicUrl').text
         self.MediaId = xmlData.find('MediaId').text
+        '''
