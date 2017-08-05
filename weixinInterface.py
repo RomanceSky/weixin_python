0c451439af41dab5fa0a6d4e7109d65e9fa8fa3b
Nobody <nobody@yq150.onlinetools>
2017-08-05 21:07:04 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -52,33 +52,10 @@ class WeixinInterface:
             else:
                 print "暂且不处理"
                 return "success"
-            '''
-            str_xml = web.data() #获得post来的数据
-            xml = etree.fromstring(str_xml)#进行XML解析
-            content=xml.find("Content").text#获得用户所输入的内容
-            msgType=xml.find("MsgType").text
-            fromUser=xml.find("FromUserName").text
-            toUser=xml.find("ToUserName").text
-            # 1
-            #recMsg = receive.parse_xml(webData)
-            #if msgType == 'text':
-            #   content = xml.find("Content").text
-                
-                 #if content == 'help': 
-                #    return self.render.reply_text(fromUser, toUser, int(time.time()), "随便看看？（对不起我功能有限QAQ）")
-           
-
-                    #return self.render.reply_text(fromUser, toUser, int(time.time()), "哎呀出错了 输入个help看看如何正确的调戏我？")
-            #if msgType == 'event':
-               # if xml.find("Event").text == 'subscribe':#关注的时候的欢迎语
-               #     return self.render.reply_text(fromUser, toUser, int(time.time()), u"谢谢你的关注，输入help看看如何正确的调戏我")
-except Exception, Argment:
-        #    return Argment
-
-'''
+            
         except Exception, Argment:
             return Argment
-        '''    
+    
         if type(content).__name__ == "unicode":
             content = xml.find('Content').text
             content = content.encode('UTF-8')
@@ -98,5 +75,3 @@ except Exception, Argment:
             return trans
         else:
             return u'对不起，您输入的单词%s无法翻译，请检查拼写'% word
-
- '''
